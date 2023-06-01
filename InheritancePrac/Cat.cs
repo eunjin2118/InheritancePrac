f6d7e0371f9b3123128aba4aba759b49fcbd0beb
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-06-01 09:02:45 +0900

    강아지, 고양이 개별 클래스 구현

---
+++ InheritancePrac/Cat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InheritancePrac
+{
+    public class Cat
+    {
+        public int Age { get; set; }
+
+        public Cat() { this.Age = 0; }
+
+        public void Meow()
+        {
+            Console.WriteLine("야옹");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("냠냠");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨");
+        }
+    }
+}
--- InheritancePrac/Program.cs
+++ InheritancePrac/Program.cs
@@ -10,6 +10,23 @@ namespace InheritancePrac
     {
         static void Main(string[] args)
         {
+            List<Dog> Dogs = new List<Dog>() {  new Dog(), new Dog(), new Dog()};
+
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
         }
     }
 }
